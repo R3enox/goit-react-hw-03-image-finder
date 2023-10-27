ab17ab71821c75d02216adf8658da85e9e23526b
R3enox <129979133+R3enox@users.noreply.github.com>
2023-10-27 23:09:33 +0300

    done task 02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Search } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
+export class App extends Component {
+  state = {
+    searchQ: '',
+  };
+  handleSearch = searchQ => {
+    this.setState({ searchQ });
+  };
+
+  render() {
+    return (
+      <>
+        <Search handleSearch={this.handleSearch} />
+        {this.state.searchQ !== null && (
+          <ImageGallery searchQ={this.state.searchQ} />
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,91 @@
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import { LoadMoreBtn } from 'components/LodaMoreBtn/LoadMoreBtn';
+import { Modal } from 'components/Modal/Modal';
+import { Component } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import css from './ImageGallery.module.css';
+import { MagnifyingGlass } from 'react-loader-spinner';
+
+const { PixabayApi } = require('components/pixabay-api/pixabay-api');
+
+export class ImageGallery extends Component {
+  state = {
+    photos: null,
+    page: 1,
+    isLoading: false,
+    error: null,
+    isShowModal: false,
+    id: null,
+    loadMore: true,
+  };
+
+  showModal = evt => {
+    this.setState({ isShowModal: true, id: evt.currentTarget.id });
+  };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false });
+  };
+  loadMore = () => {
+    const pixabayApi = new PixabayApi(12);
+    pixabayApi.q = this.props.searchQ;
+    pixabayApi.page = this.state.page + 1;
+    pixabayApi.getContent().then(({ hits, totalHits }) =>
+      this.setState(prev => ({
+        photos: [...prev.photos, ...hits],
+        page: prev.page + 1,
+        loadMore: this.state.page < Math.floor(totalHits / 12),
+      }))
+    );
+  };
+
+  componentDidUpdate(prevProps, _) {
+    const pixabayApi = new PixabayApi(12);
+    pixabayApi.q = this.props.searchQ;
+    if (prevProps.searchQ !== this.props.searchQ) {
+      pixabayApi
+        .getContent()
+        .then(({ hits, totalHits }) => {
+          this.setState({ photos: hits, isLoading: true });
+          if (totalHits === 0) {
+            return Notify.failure(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
+          } else {
+            Notify.success(`Hooray! We found ${totalHits} images.`);
+          }
+        })
+        .catch(error => this.setState({ error: error.message }))
+        .finally(
+          this.setState({
+            isLoading: false,
+          })
+        );
+    }
+  }
+  render() {
+    return (
+      <ul className={css.gallery}>
+        {this.state.photos !== null && (
+          <>
+            {this.state.isLoading && <MagnifyingGlass />}
+            <ImageGalleryItem
+              photos={this.state.photos}
+              showModal={this.showModal}
+            />
+            {this.state.photos.length > 0 && this.state.loadMore && (
+              <LoadMoreBtn loadMore={this.loadMore} />
+            )}
+            {this.state.isShowModal && (
+              <Modal
+                photos={this.state.photos}
+                id={this.state.id}
+                closeModal={this.closeModal}
+              />
+            )}
+          </>
+        )}
+      </ul>
+    );
+  }
+}
